7e2a4036396bc2492363abeeb257e8117b900791
Zanzo <zp2kshield@gmail.com>
2018-08-14 07:50:51 -0700

    init upload

---
+++ stdafx.cpp
@@ -0,0 +1,7 @@
+// stdafx.cpp : source file that includes just the standard includes
+// SoT-DataSniffer.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+
